c8c031561762d85485392958cbc98cb3bb1e6384
Polyakov Anton <polyakov.a.a.03@mail.ru>
2025-09-04 21:50:53 +0700

    init frontend with cors request to backend

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted } from 'vue'
+import AssetsTable from './components/AssetsTable.vue'
+import { ref } from 'vue'
+import axios from 'axios'
+
+const assetsData = ref([])
+
+const getAllAssets = async () => {
+  const { data } = await axios.get('http://localhost:5001/api/assets/getAssets') //в будущем добавить query params
+  return data
+}
+
+onMounted(async () => {
+  assetsData.value = await getAllAssets()
+})
+</script>
+
+<template>
+  <AssetsTable />
+</template>
+
+<style scoped></style>
